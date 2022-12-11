dfa7a6d8441577c3161e979ac18d163dd8d611d9
AndzelinaWejt <100134643+AndzelinaWejt@users.noreply.github.com>
2022-12-11 15:38:15 +0100

    feat: configured injectable for flavor

--- lib/injectable/injectable.dart
+++ lib/injectable/injectable.dart
@@ -1,9 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:recipe_finder/injectable/enviroment.dart';
 
 import 'injectable.config.dart';
 
 final getIt = GetIt.instance;
 
 @InjectableInit()
-Future<void> configureDependencies() async => $initGetIt(getIt);
+Future<void> configureDependencies({
+  String environment = EnvironmentName.development,
+}) async =>
+    $initGetIt(
+      getIt,
+      environment: environment,
+    );
