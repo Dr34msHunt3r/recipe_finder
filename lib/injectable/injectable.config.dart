4ac4f240104dd573651650f2dcae34117d377636
AndzelinaWejt <100134643+AndzelinaWejt@users.noreply.github.com>
2023-01-12 10:53:39 +0100

    rerun build runner

--- lib/injectable/injectable.config.dart
+++ lib/injectable/injectable.config.dart
@@ -5,34 +5,44 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:firebase_analytics/firebase_analytics.dart' as _i3;
-import 'package:firebase_crashlytics/firebase_crashlytics.dart' as _i5;
+import 'package:firebase_analytics/firebase_analytics.dart' as _i7;
+import 'package:firebase_crashlytics/firebase_crashlytics.dart' as _i9;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:logger/logger.dart' as _i7;
+import 'package:logger/logger.dart' as _i12;
 
-import '../data_source/analytics/analytics_tracker.dart' as _i13;
-import '../data_source/analytics/default_analytics_repository.dart' as _i16;
+import '../core/config/app/app_config.dart' as _i3;
+import '../core/config/app/dev_app_config.dart' as _i4;
+import '../core/config/app/prod_app_config.dart' as _i5;
+import '../core/config/app/staging_app_config.dart' as _i6;
+import '../data_source/analytics/analytics_tracker.dart' as _i18;
+import '../data_source/analytics/default_analytics_repository.dart' as _i21;
 import '../data_source/analytics/trackers/firebase_analytics_tracker.dart'
-    as _i4;
+    as _i8;
 import '../data_source/analytics/trackers/std_out_analytics_tracker.dart'
-    as _i11;
-import '../data_source/crashlytics/crashlytics_repository.dart' as _i18;
-import '../data_source/crashlytics/error_tracker.dart' as _i14;
-import '../data_source/crashlytics/trackers/firebase_error_tracker.dart' as _i6;
-import '../data_source/crashlytics/trackers/std_out_error_tracker.dart' as _i12;
-import '../domain/analytics/analytics_repository.dart' as _i15;
-import '../domain/analytics/event_tracker_use_case.dart' as _i20;
-import '../domain/analytics/screen_tracker_use_case.dart' as _i21;
-import '../domain/crashlytics/crashlytics_repository.dart' as _i17;
-import '../domain/crashlytics/error_tracker_use_case.dart' as _i19;
-import '../features/scanner/presentation/cubit/scanner_cubit.dart' as _i8;
-import '../features/settings/presentation/cubit/settings_cubit.dart' as _i9;
-import '../features/splash_screen/presentation/cubit/splash_cubit.dart' as _i10;
-import 'modules/analytics.dart' as _i22;
-import 'modules/crashlytics.dart' as _i23;
-import 'modules/logger.dart' as _i24; // ignore_for_file: unnecessary_lambdas
+    as _i16;
+import '../data_source/crashlytics/crashlytics_repository.dart' as _i23;
+import '../data_source/crashlytics/error_tracker.dart' as _i19;
+import '../data_source/crashlytics/trackers/firebase_error_tracker.dart'
+    as _i10;
+import '../data_source/crashlytics/trackers/std_out_error_tracker.dart' as _i17;
+import '../domain/analytics/analytics_repository.dart' as _i20;
+import '../domain/analytics/event_tracker_use_case.dart' as _i25;
+import '../domain/analytics/screen_tracker_use_case.dart' as _i26;
+import '../domain/crashlytics/crashlytics_repository.dart' as _i22;
+import '../domain/crashlytics/error_tracker_use_case.dart' as _i24;
+import '../features/home/presentation/cubit/home_cubit.dart' as _i11;
+import '../features/scanner/presentation/cubit/scanner_cubit.dart' as _i13;
+import '../features/settings/presentation/cubit/settings_cubit.dart' as _i14;
+import '../features/splash_screen/presentation/cubit/splash_cubit.dart' as _i15;
+import 'modules/analytics.dart' as _i27;
+import 'modules/crashlytics.dart' as _i28;
+import 'modules/logger.dart' as _i29;
 
+const String _development = 'development';
+const String _production = 'production';
+const String _staging = 'staging';
+// ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(
@@ -48,51 +58,64 @@ _i1.GetIt $initGetIt(
   final analyticsModule = _$AnalyticsModule();
   final crashlyticsModule = _$CrashlyticsModule();
   final loggerModule = _$LoggerModule();
-  gh.factory<_i3.FirebaseAnalytics>(
+  gh.factory<_i3.AppConfig>(
+    () => _i4.DevelopmentAppConfig(),
+    registerFor: {_development},
+  );
+  gh.factory<_i3.AppConfig>(
+    () => _i5.ProductionAppConfig(),
+    registerFor: {_production},
+  );
+  gh.factory<_i3.AppConfig>(
+    () => _i6.StagingAppConfig(),
+    registerFor: {_staging},
+  );
+  gh.factory<_i7.FirebaseAnalytics>(
       () => analyticsModule.getFirebaseAnalytics());
-  gh.factory<_i4.FirebaseAnalyticsTracker>(() =>
-      _i4.FirebaseAnalyticsTracker(analytics: get<_i3.FirebaseAnalytics>()));
-  gh.factory<_i5.FirebaseCrashlytics>(
+  gh.factory<_i8.FirebaseAnalyticsTracker>(() =>
+      _i8.FirebaseAnalyticsTracker(analytics: get<_i7.FirebaseAnalytics>()));
+  gh.factory<_i9.FirebaseCrashlytics>(
       () => crashlyticsModule.getFirebaseCrashlytics());
-  gh.factory<_i6.FirebaseErrorTracker>(() =>
-      _i6.FirebaseErrorTracker(crashlytics: get<_i5.FirebaseCrashlytics>()));
-  gh.factory<_i7.Logger>(() => loggerModule.getLogger());
-  gh.factory<_i8.ScannerCubit>(() => _i8.ScannerCubit());
-  gh.factory<_i9.SettingsCubit>(() => _i9.SettingsCubit());
-  gh.factoryParam<_i10.SplashCubit, List<_i10.LoadingTask>?, dynamic>((
+  gh.factory<_i10.FirebaseErrorTracker>(() =>
+      _i10.FirebaseErrorTracker(crashlytics: get<_i9.FirebaseCrashlytics>()));
+  gh.factory<_i11.HomeCubit>(() => _i11.HomeCubit());
+  gh.factory<_i12.Logger>(() => loggerModule.getLogger());
+  gh.factory<_i13.ScannerCubit>(() => _i13.ScannerCubit());
+  gh.factory<_i14.SettingsCubit>(() => _i14.SettingsCubit());
+  gh.factoryParam<_i15.SplashCubit, List<_i15.LoadingTask>?, dynamic>((
     loadingTasks,
     _,
   ) =>
-      _i10.SplashCubit(loadingTasks: loadingTasks));
-  gh.factory<_i11.StdOutAnalyticsTracker>(
-      () => _i11.StdOutAnalyticsTracker(logger: get<_i7.Logger>()));
-  gh.factory<_i12.StdOutErrorTracker>(
-      () => _i12.StdOutErrorTracker(logger: get<_i7.Logger>()));
-  gh.factory<List<_i13.AnalyticsTracker>>(
+      _i15.SplashCubit(loadingTasks: loadingTasks));
+  gh.factory<_i16.StdOutAnalyticsTracker>(
+      () => _i16.StdOutAnalyticsTracker(logger: get<_i12.Logger>()));
+  gh.factory<_i17.StdOutErrorTracker>(
+      () => _i17.StdOutErrorTracker(logger: get<_i12.Logger>()));
+  gh.factory<List<_i18.AnalyticsTracker>>(
       () => analyticsModule.getAnalyticsTrackers(
-            get<_i4.FirebaseAnalyticsTracker>(),
-            get<_i11.StdOutAnalyticsTracker>(),
+            get<_i8.FirebaseAnalyticsTracker>(),
+            get<_i16.StdOutAnalyticsTracker>(),
           ));
-  gh.factory<List<_i14.ErrorTracker>>(() => crashlyticsModule.getErrorTrackers(
-        get<_i6.FirebaseErrorTracker>(),
-        get<_i12.StdOutErrorTracker>(),
+  gh.factory<List<_i19.ErrorTracker>>(() => crashlyticsModule.getErrorTrackers(
+        get<_i10.FirebaseErrorTracker>(),
+        get<_i17.StdOutErrorTracker>(),
       ));
-  gh.factory<_i15.AnalyticsRepository>(() => _i16.DefaultAnalyticsRepository(
-      trackers: get<List<_i13.AnalyticsTracker>>()));
-  gh.factory<_i17.CrashlyticsRepository>(() =>
-      _i18.DefaultCrashlyticsRepository(
-          trackers: get<List<_i14.ErrorTracker>>()));
-  gh.factory<_i19.ErrorTrackerUseCase>(() => _i19.ErrorTrackerUseCase(
-      crashlyticsRepository: get<_i17.CrashlyticsRepository>()));
-  gh.factory<_i20.EventTrackerUseCase>(() => _i20.EventTrackerUseCase(
-      analyticsRepository: get<_i15.AnalyticsRepository>()));
-  gh.factory<_i21.ScreenTrackerUseCase>(() => _i21.ScreenTrackerUseCase(
-      analyticsRepository: get<_i15.AnalyticsRepository>()));
+  gh.factory<_i20.AnalyticsRepository>(() => _i21.DefaultAnalyticsRepository(
+      trackers: get<List<_i18.AnalyticsTracker>>()));
+  gh.factory<_i22.CrashlyticsRepository>(() =>
+      _i23.DefaultCrashlyticsRepository(
+          trackers: get<List<_i19.ErrorTracker>>()));
+  gh.factory<_i24.ErrorTrackerUseCase>(() => _i24.ErrorTrackerUseCase(
+      crashlyticsRepository: get<_i22.CrashlyticsRepository>()));
+  gh.factory<_i25.EventTrackerUseCase>(() => _i25.EventTrackerUseCase(
+      analyticsRepository: get<_i20.AnalyticsRepository>()));
+  gh.factory<_i26.ScreenTrackerUseCase>(() => _i26.ScreenTrackerUseCase(
+      analyticsRepository: get<_i20.AnalyticsRepository>()));
   return get;
 }
 
-class _$AnalyticsModule extends _i22.AnalyticsModule {}
+class _$AnalyticsModule extends _i27.AnalyticsModule {}
 
-class _$CrashlyticsModule extends _i23.CrashlyticsModule {}
+class _$CrashlyticsModule extends _i28.CrashlyticsModule {}
 
-class _$LoggerModule extends _i24.LoggerModule {}
+class _$LoggerModule extends _i29.LoggerModule {}
