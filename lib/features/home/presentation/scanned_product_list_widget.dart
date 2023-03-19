de50b28314469549158fdbd63c4dc41cc0dc383a
AndzelinaWejt <100134643+AndzelinaWejt@users.noreply.github.com>
2023-03-19 15:50:53 +0100

    feat: product list

---
+++ lib/features/home/presentation/scanned_product_list_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_finder/core/config/app_colors.dart';
+import 'package:recipe_finder/core/config/app_dimens.dart';
+
+class ScannedProductListWidget extends StatelessWidget {
+  const ScannedProductListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) => const ScannedProductListView();
+}
+
+class ScannedProductListView extends StatelessWidget {
+  const ScannedProductListView({super.key});
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.only(
+          top: AppDimens.subLargerSpace_20,
+        ),
+        child: SizedBox(
+          width: 200,
+          child: Column(
+            children: const [
+              Text(
+                'Item No. 1',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+              Divider(),
+              Text(
+                'Item No. 2',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+              Divider(),
+              Text(
+                'Item No. 3',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+              Divider(),
+              Text(
+                'Item No. 4',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+              Divider(),
+              Text(
+                'Item No. 5',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+              Divider(),
+              Text(
+                'Item No. 6',
+                style: TextStyle(
+                  fontSize: AppDimens.normalTextSize_16,
+                  color: AppColors.primaryText,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
