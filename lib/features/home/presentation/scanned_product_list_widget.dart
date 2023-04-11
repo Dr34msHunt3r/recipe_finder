21190faa42109107859318f531be06c0c4105d8f
AndzelinaWejt <100134643+AndzelinaWejt@users.noreply.github.com>
2023-04-11 16:02:22 +0200

    feat: custom scroll behavior

--- lib/features/home/presentation/scanned_product_list_widget.dart
+++ lib/features/home/presentation/scanned_product_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_finder/common/widgets/custom_scroll_behavior.dart';
 import 'package:recipe_finder/core/config/app_colors.dart';
 import 'package:recipe_finder/core/config/app_dimens.dart';
 
@@ -20,57 +21,60 @@ class ScannedProductListView extends StatelessWidget {
         child: SizedBox(
           width: 200,
           height: 150,
-          child: SingleChildScrollView(
-            child: Column(
-              children: const [
-                Text(
-                  'Item No. 1',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+          child: ScrollConfiguration(
+            behavior: CustomScrollBehavior(),
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  Text(
+                    'Item No. 1',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-                Divider(),
-                Text(
-                  'Item No. 2',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+                  Divider(),
+                  Text(
+                    'Item No. 2',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-                Divider(),
-                Text(
-                  'Item No. 3',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+                  Divider(),
+                  Text(
+                    'Item No. 3',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-                Divider(),
-                Text(
-                  'Item No. 4',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+                  Divider(),
+                  Text(
+                    'Item No. 4',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-                Divider(),
-                Text(
-                  'Item No. 5',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+                  Divider(),
+                  Text(
+                    'Item No. 5',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-                Divider(),
-                Text(
-                  'Item No. 6',
-                  style: TextStyle(
-                    fontSize: AppDimens.normalTextSize_16,
-                    color: AppColors.primaryText,
+                  Divider(),
+                  Text(
+                    'Item No. 6',
+                    style: TextStyle(
+                      fontSize: AppDimens.normalTextSize_16,
+                      color: AppColors.primaryText,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
